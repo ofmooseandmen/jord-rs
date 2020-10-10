8609e40f003f3ca71f948016768f0c78946e4c81
Cedric Liegeois <ofmooseandmen@yahoo.com>
2020-10-10 19:42:40 +1100

    more surfaces

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,87 +1,233 @@
 use std::env;
 use std::fs::File;
-use std::io::Write;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Error, ErrorKind, Write};
 use std::path::Path;
 
 use jord::{Ellipsoid, Length, Sphere, Surface};
 
-fn gen_surfaces(es: Vec<(String, Box<dyn Surface>)>, f: &str) -> std::io::Result<()> {
+fn gen_surfaces(comments: Vec<String>, surfaces: Vec<Surf>, f: &str) -> io::Result<()> {
     let mut file = File::create(f)?;
-    file.write("use crate::{Ellipsoid, Length, Sphere};\n".as_bytes())?;
-    file.write("\n".as_bytes())?;
-    for e in es {
-        let s;
-        if e.1.flattening() == 0.0 {
-            s = gen_sphere(e.0, e.1);
-        } else {
-            s = gen_ellispoid(e.0, e.1);
-        }
-        file.write_all(s.as_bytes())?;
+    file.write_all("// Copyright: (c) 2020 Cedric Liegeois\n// License: BSD3".as_bytes())?;
+    write_new_line(&mut file)?;
+    write_new_line(&mut file)?;
+
+    write_comments(&comments, &mut file)?;
+    write_new_line(&mut file)?;
+    file.write_all("use crate::{Ellipsoid, Length, Sphere};\n".as_bytes())?;
+    write_new_line(&mut file)?;
+
+    for surface in surfaces {
+        let txt = match surface {
+            Surf::Ellipsoid {
+                comment,
+                name,
+                data,
+            } => gen_ellispoid(comment, name, data),
+            Surf::Sphere {
+                comment,
+                name,
+                data,
+            } => gen_sphere(comment, name, data),
+        };
+        file.write_all(txt.as_bytes())?;
     }
     Ok(())
 }
 
-fn gen_sphere(n: String, e: Box<dyn Surface>) -> String {
+fn write_new_line(file: &mut File) -> io::Result<()> {
+    file.write_all(b"\n")?;
+    Ok(())
+}
+
+fn write_comments(comments: &[String], file: &mut File) -> io::Result<()> {
+    for c in comments {
+        file.write_all(("//! ".to_owned() + &c).as_bytes())?;
+        write_new_line(file)?;
+        file.write_all(b"//! ")?;
+    }
+    write_new_line(file)?;
+    Ok(())
+}
+
+fn gen_sphere(c: String, n: String, e: Sphere) -> String {
     format!(
-        "pub const {}: Sphere = Sphere::new(Length::from_micrometres({}));
+        "/// {}
+pub const {}: Sphere = Sphere::new(Length::from_micrometres({}));
 
 ",
+        c,
         n.to_uppercase(),
         e.mean_radius().micrometres(),
     )
 }
 
-fn gen_ellispoid(n: String, e: Box<dyn Surface>) -> String {
+fn gen_ellispoid(c: String, n: String, e: Ellipsoid) -> String {
     format!(
-        "pub const {}: Ellipsoid =  Ellipsoid::from_all(
+        "/// {}
+pub const {}: Ellipsoid =  Ellipsoid::from_all(
     Length::from_micrometres({}),
     Length::from_micrometres({}),
     {},
     {},
 );
 
+/// {}
 pub const {}: Sphere = Sphere::new(Length::from_micrometres({}));
 
 ",
+        c,
         n.to_uppercase(),
         e.equatorial_radius().micrometres(),
         e.polar_radius().micrometres(),
         e.eccentricity(),
         e.flattening(),
+        "Sphere derived from: ".to_owned() + &c,
         n.to_uppercase() + "_SPHERE",
         e.mean_radius().micrometres(),
     )
 }
 
-fn parse_surface(e: &str) -> (String, Box<dyn Surface>) {
-    let mut iter = e.split_ascii_whitespace();
-    let name = iter.next().expect("Expected name").to_string();
-    let r = iter
-        .next()
-        .expect("Expected (equatorial) radius")
-        .parse::<f64>()
-        .unwrap();
-    let radius = Length::from_metres(r);
+enum Surf {
+    Sphere {
+        comment: String,
+        name: String,
+        data: Sphere,
+    },
+    Ellipsoid {
+        comment: String,
+        name: String,
+        data: Ellipsoid,
+    },
+}
 
-    let next = iter.next();
-    match next {
-        Some(invf) => (
-            name,
-            Box::new(Ellipsoid::new(radius, invf.parse::<f64>().unwrap())),
-        ),
-        None => (name, Box::new(Sphere::new(radius))),
+fn parse_surface(text: &Text) -> io::Result<(Surf, Text)> {
+    let txt = text.skip_empty();
+    let (comment, txt) = txt.next_if_prefixed("# ")?;
+    let (name, txt) = txt.next()?;
+    match txt.next_if_prefixed("  a: ") {
+        Err(_) => {
+            let (r, txt) = txt.next_if_prefixed("  r: ")?;
+            Ok((
+                Surf::Sphere {
+                    comment,
+                    name,
+                    data: Sphere::new(parse_metres(r)?),
+                },
+                txt,
+            ))
+        }
+        Ok((a, txt)) => {
+            let (invf, txt) = txt.next_if_prefixed("  1/f: ")?;
+            Ok((
+                Surf::Ellipsoid {
+                    comment,
+                    name,
+                    data: Ellipsoid::new(parse_metres(a)?, invf.parse::<f64>().unwrap()),
+                },
+                txt,
+            ))
+        }
     }
 }
 
-fn parse_surfaces(lines: io::Lines<io::BufReader<File>>) -> Vec<(String, Box<dyn Surface>)> {
-    let mut vec = Vec::new();
-    for line in lines {
-        if let Ok(e) = line {
-            vec.push(parse_surface(&e));
+fn parse_metres(s: String) -> io::Result<Length> {
+    let last = s.chars().last().unwrap();
+    match last {
+        'm' => Ok(Length::from_metres(
+            s[..s.len() - 1].parse::<f64>().unwrap(),
+        )),
+        _ => Err(Error::new(ErrorKind::InvalidData, "expected 'm'")),
+    }
+}
+
+#[derive(Debug)]
+struct Text(Vec<String>);
+
+impl Text {
+    fn from_file_content<P>(filename: P) -> io::Result<Text>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(filename)?;
+        let content = io::BufReader::new(file).lines();
+        let lines = content.filter_map(Result::ok).collect();
+        Ok(Text(lines))
+    }
+
+    fn is_eot(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    fn skip_empty(&self) -> Self {
+        Text(
+            self.0
+                .iter()
+                .skip_while(|l| l.is_empty())
+                .map(|s| s.to_string())
+                .collect(),
+        )
+    }
+
+    fn next(&self) -> io::Result<(String, Text)> {
+        match self.0.first() {
+            None => Err(Error::new(ErrorKind::UnexpectedEof, "expected more")),
+            Some(s) => Ok((s.to_string(), Text(self.0[1..].to_vec()))),
         }
     }
-    vec
+
+    fn next_if_prefixed(&self, prefix: &str) -> io::Result<(String, Text)> {
+        match self.0.first().and_then(|s| s.strip_prefix(prefix)) {
+            None => Err(Error::new(
+                ErrorKind::UnexpectedEof,
+                format!("expected {}, found {:?}", prefix, self.0.first()),
+            )),
+            Some(s) => Ok((s.to_string(), Text(self.0[1..].to_vec()))),
+        }
+    }
+}
+
+fn parse_surfaces(text: Text) -> io::Result<(Vec<String>, String, Vec<Surf>)> {
+    let (comments, txt) = parse_comments(text);
+    match parse_module(txt) {
+        Err(e) => Err(e),
+        Ok((module, txt)) => {
+            let mut vec = Vec::new();
+            let mut done = false;
+            let mut rest = txt;
+            while !done {
+                let (s, t) = parse_surface(&rest)?;
+                rest = t;
+                vec.push(s);
+                rest = rest.skip_empty();
+                done = rest.is_eot();
+            }
+            Ok((comments, module, vec))
+        }
+    }
+}
+
+fn parse_comments(text: Text) -> (Vec<String>, Text) {
+    let mut txt = text.skip_empty();
+    let mut comments = Vec::new();
+    let mut done = false;
+    while !done {
+        match txt.next_if_prefixed("# ") {
+            Err(_) => {
+                done = true;
+            }
+            Ok((cmt, rtxt)) => {
+                comments.push(cmt.to_string());
+                txt = rtxt
+            }
+        }
+    }
+    (comments, txt)
+}
+
+fn parse_module(text: Text) -> io::Result<(String, Text)> {
+    let txt = text.skip_empty();
+    let res = txt.next_if_prefixed("mod ")?;
+    Ok(res)
 }
 
 pub fn main() -> std::io::Result<()> {
@@ -95,21 +241,11 @@ pub fn main() -> std::io::Result<()> {
     let mut in_surfaces = in_dir.to_owned();
     in_surfaces.push_str("/surfaces.txt");
 
-    let lines = read_lines(in_surfaces)?;
-    let surfaces = parse_surfaces(lines);
+    let text = Text::from_file_content(in_surfaces)?;
+    let surfaces = parse_surfaces(text)?;
 
     let mut out_surfaces = out_dir.to_owned();
-    out_surfaces.push_str("/surfaces.rs");
+    out_surfaces.push_str(&format!("/{}.rs", surfaces.1));
 
-    gen_surfaces(surfaces, &out_surfaces)
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    gen_surfaces(surfaces.0, surfaces.2, &out_surfaces)
 }
--- src/surfaces.rs
+++ src/surfaces.rs
@@ -1,21 +1,121 @@
+// Copyright: (c) 2020 Cedric Liegeois
+// License: BSD3
+
+//! Common surfaces of different celestial bodies
+//! 
+
 use crate::{Ellipsoid, Length, Sphere};
 
-pub const WGS84: Ellipsoid = Ellipsoid::from_all(
+/// World Geodetic 84 Ellipsoid.
+pub const WGS84: Ellipsoid =  Ellipsoid::from_all(
     Length::from_micrometres(6378137000000),
     Length::from_micrometres(6356752314245),
     0.08181919084262157,
     0.0033528106647474805,
 );
 
+/// Sphere derived from: World Geodetic 84 Ellipsoid.
 pub const WGS84_SPHERE: Sphere = Sphere::new(Length::from_micrometres(6371008771415));
 
-pub const MARS_2000: Ellipsoid = Ellipsoid::from_all(
+/// Geodetic Reference System 1980 Ellipsoid.
+pub const GRS80: Ellipsoid =  Ellipsoid::from_all(
+    Length::from_micrometres(6378137000000),
+    Length::from_micrometres(6356752314140),
+    0.08181919104281514,
+    0.003352810681182319,
+);
+
+/// Sphere derived from: Geodetic Reference System 1980 Ellipsoid.
+pub const GRS80_SPHERE: Sphere = Sphere::new(Length::from_micrometres(6371008771380));
+
+/// World Geodetic 72 Ellipsoid.
+pub const WGS72: Ellipsoid =  Ellipsoid::from_all(
+    Length::from_micrometres(6378135000000),
+    Length::from_micrometres(6356750520016),
+    0.08181881066274845,
+    0.003352779454167505,
+);
+
+/// Sphere derived from: World Geodetic 72 Ellipsoid.
+pub const WGS72_SPHERE: Sphere = Sphere::new(Length::from_micrometres(6371006840005));
+
+/// IUGG 1924 Ellipsoid (aka Hayford).
+pub const INTL_1924: Ellipsoid =  Ellipsoid::from_all(
+    Length::from_micrometres(6378388000000),
+    Length::from_micrometres(6356911946128),
+    0.08199188997902888,
+    0.003367003367003367,
+);
+
+/// Sphere derived from: IUGG 1924 Ellipsoid (aka Hayford).
+pub const INTL_1924_SPHERE: Sphere = Sphere::new(Length::from_micrometres(6371229315376));
+
+/// Original definition Ellipsoid (1796).
+pub const AIRY_1830: Ellipsoid =  Ellipsoid::from_all(
+    Length::from_micrometres(6377563396000),
+    Length::from_micrometres(6356256909237),
+    0.08167337387414043,
+    0.0033408506414970775,
+);
+
+/// Sphere derived from: Original definition Ellipsoid (1796).
+pub const AIRY_1830_SPHERE: Sphere = Sphere::new(Length::from_micrometres(6370461233746));
+
+/// Not specified, use only in cases where geodetic datum is unknown.
+pub const AIRY_MODIFIED: Ellipsoid =  Ellipsoid::from_all(
+    Length::from_micrometres(6377340189000),
+    Length::from_micrometres(6356034447939),
+    0.08167337387414247,
+    0.0033408506414970775,
+);
+
+/// Sphere derived from: Not specified, use only in cases where geodetic datum is unknown.
+pub const AIRY_MODIFIED_SPHERE: Sphere = Sphere::new(Length::from_micrometres(6370238275313));
+
+/// Bessel 1841 Ellipsoid.
+pub const BESSEL_1841: Ellipsoid =  Ellipsoid::from_all(
+    Length::from_micrometres(6377397155000),
+    Length::from_micrometres(6356078962818),
+    0.08169683122252666,
+    0.003342773182174806,
+);
+
+/// Sphere derived from: Bessel 1841 Ellipsoid.
+pub const BESSEL_1841_SPHERE: Sphere = Sphere::new(Length::from_micrometres(6370291090939));
+
+/// Clarke (1866) Ellipsoid.
+pub const CLARKE_1866: Ellipsoid =  Ellipsoid::from_all(
+    Length::from_micrometres(6378206400000),
+    Length::from_micrometres(6356583800000),
+    0.08227185422298973,
+    0.0033900753039276207,
+);
+
+/// Sphere derived from: Clarke (1866) Ellipsoid.
+pub const CLARKE_1866_SPHERE: Sphere = Sphere::new(Length::from_micrometres(6370998866667));
+
+///  Clarke (1880) Ellipsoid.
+pub const CLARKE_1880_IGN: Ellipsoid =  Ellipsoid::from_all(
+    Length::from_micrometres(6378249200000),
+    Length::from_micrometres(6356515000000),
+    0.08248325676336525,
+    0.003407549520011315,
+);
+
+/// Sphere derived from:  Clarke (1880) Ellipsoid.
+pub const CLARKE_1880_IGN_SPHERE: Sphere = Sphere::new(Length::from_micrometres(6371004466667));
+
+/// Mars Orbiter Laser Altimeter Ellipsoid.
+pub const MARS_2000: Ellipsoid =  Ellipsoid::from_all(
     Length::from_micrometres(3398627000000),
     Length::from_micrometres(3378611528857),
     0.10836918094474898,
     0.005889281507656065,
 );
 
+/// Sphere derived from: Mars Orbiter Laser Altimeter Ellipsoid.
 pub const MARS_2000_SPHERE: Sphere = Sphere::new(Length::from_micrometres(3391955176286));
 
+/// Moon IAU/IAG Sphere.
 pub const MOON: Sphere = Sphere::new(Length::from_micrometres(1737400000000));
+
