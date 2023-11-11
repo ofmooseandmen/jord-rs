a4dd2163c266bc8d0814ad37d5b365efc1cfc57d
Cedric Liegeois <omam.github@gmail.com>
2023-11-11 14:29:58 +1100

    rectangle cmp + tests
    also 0.3.0

--- src/spherical/rectangle.rs
+++ src/spherical/rectangle.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use crate::{
     numbers::{eq_zero, gte, lte},
     Angle, LatLong, Vec3,
@@ -181,6 +183,31 @@ impl Rectangle {
             *self
         }
     }
+    /// Compares the latitude intervalsof this rectangle and the given one: the [greater](Ordering::Greater) latitude interval is defined as the
+    ///  interval that is northernmost overall (including both low and high latitudes).
+    pub fn cmp_by_latitude(&self, o: Self) -> Ordering {
+        let s = (self.lat.lo + self.lat.hi) - (o.lat.lo + o.lat.hi);
+        if s == Angle::ZERO {
+            Ordering::Equal
+        } else if s < Angle::ZERO {
+            Ordering::Less
+        } else {
+            Ordering::Greater
+        }
+    }
+
+    /// Compares the longitude intervals of this rectangle and the given one: the [greater](Ordering::Greater) longitude interval is defined as
+    /// the interval that is easternmost overall (including both low and high longitudes).
+    pub fn cmp_by_longitude(&self, o: Self) -> Ordering {
+        let s = (self.lng.lo + self.lng.hi) - (o.lng.lo + o.lng.hi);
+        if s == Angle::ZERO {
+            Ordering::Equal
+        } else if s < Angle::ZERO {
+            Ordering::Less
+        } else {
+            Ordering::Greater
+        }
+    }
 }
 
 /// latitude interval: {@link #lo} is assumed to be less than {@link #hi}, otherwise the interval is empty.
@@ -350,8 +377,283 @@ impl LongitudeInterval {
     fn is_empty(&self) -> bool {
         self.lo == Angle::HALF_CIRCLE && self.hi == -Angle::HALF_CIRCLE
     }
+
     /// Returns true if this longitude interval is inverted.
     fn is_inverted(&self) -> bool {
         self.lo > self.hi
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cmp::Ordering;
+
+    use crate::{Angle, LatLong};
+
+    use super::Rectangle;
+
+    // cmp_by_latitude
+
+    #[test]
+    fn cmp_by_latitude_eq() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(30.0),
+            Angle::ZERO,
+            Angle::ZERO,
+        );
+        let b = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(20.0),
+            Angle::ZERO,
+            Angle::from_degrees(10.0),
+        );
+        assert_eq!(Ordering::Equal, a.cmp_by_latitude(b));
+        assert_eq!(Ordering::Equal, b.cmp_by_latitude(a));
+    }
+
+    #[test]
+    fn cmp_by_latitude_northernmost_in_north_hemisphere() {
+        // latitude interval of a = 0 to 30 degrees.
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(30.0),
+            Angle::ZERO,
+            Angle::ZERO,
+        );
+        // latitude interval of b = 0 to 20 degrees.
+        let b = Rectangle::from_nesw(
+            Angle::from_degrees(20.0),
+            Angle::from_degrees(20.0),
+            Angle::ZERO,
+            Angle::from_degrees(10.0),
+        );
+
+        assert_eq!(Ordering::Greater, a.cmp_by_latitude(b));
+        assert_eq!(Ordering::Less, b.cmp_by_latitude(a));
+    }
+
+    #[test]
+    fn cmp_by_latitude_northernmost_in_south_hemisphere() {
+        // latitude interval of a = -30 to -10 degrees.
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(-10.0),
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(-30.0),
+            Angle::ZERO,
+        );
+        // latitude interval of b = -30 to -20 degrees.
+        let b = Rectangle::from_nesw(
+            Angle::from_degrees(-20.0),
+            Angle::from_degrees(20.0),
+            Angle::from_degrees(-30.0),
+            Angle::from_degrees(10.0),
+        );
+
+        assert_eq!(Ordering::Greater, a.cmp_by_latitude(b));
+        assert_eq!(Ordering::Less, b.cmp_by_latitude(a));
+    }
+
+    // cmp_by_longitude
+
+    #[test]
+    fn cmp_by_longitude_eq() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(20.0),
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(10.0),
+            Angle::ZERO,
+        );
+        let b = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(20.0),
+            Angle::ZERO,
+        );
+        assert_eq!(Ordering::Equal, a.cmp_by_longitude(b));
+        assert_eq!(Ordering::Equal, b.cmp_by_longitude(a));
+    }
+
+    #[test]
+    fn cmp_by_longitude_easternmost_longitude_is_negative() {
+        // longitude interval of a = -40 to -20 degrees.
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(20.0),
+            Angle::from_degrees(-20.0),
+            Angle::from_degrees(10.0),
+            Angle::from_degrees(-40.0),
+        );
+        //longitude interval of b = -40 to -30 degrees.
+        let b = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(-30.0),
+            Angle::from_degrees(20.0),
+            Angle::from_degrees(-40.0),
+        );
+        assert_eq!(Ordering::Greater, a.cmp_by_longitude(b));
+        assert_eq!(Ordering::Less, b.cmp_by_longitude(a));
+    }
+
+    #[test]
+    fn cmp_by_longitude_easternmost_longitude_is_positive() {
+        //longitude interval of a = 0 to 30 degrees.
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(20.0),
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(10.0),
+            Angle::ZERO,
+        );
+        //longitude interval of b = 0 to 20 degrees.
+        let b = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(20.0),
+            Angle::from_degrees(20.0),
+            Angle::ZERO,
+        );
+        assert_eq!(Ordering::Greater, a.cmp_by_longitude(b));
+        assert_eq!(Ordering::Less, b.cmp_by_longitude(a));
+    }
+
+    // contains_point
+
+    #[test]
+    fn contains_point_east() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(30.0),
+            Angle::ZERO,
+            Angle::ZERO,
+        );
+        assert!(a.contains_point(LatLong::from_degrees(10.0, 30.0)));
+    }
+
+    #[test]
+    fn contains_point_north() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(30.0),
+            Angle::ZERO,
+            Angle::ZERO,
+        );
+        assert!(a.contains_point(LatLong::from_degrees(30.0, 20.0)));
+    }
+
+    #[test]
+    fn contains_point_north_pole() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(90.0),
+            Angle::from_degrees(30.0),
+            Angle::ZERO,
+            Angle::ZERO,
+        );
+        let np: LatLong = LatLong::from_degrees(90.0, 160.0);
+        assert!(!a.contains_point(np));
+        assert!(a.polar_closure().contains_point(np));
+    }
+
+    #[test]
+    fn contains_point_south() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(30.0),
+            Angle::ZERO,
+            Angle::ZERO,
+        );
+        assert!(a.contains_point(LatLong::from_degrees(0.0, 20.0)));
+    }
+
+    #[test]
+    fn contains_south_pole() {
+        let a = Rectangle::from_nesw(
+            Angle::ZERO,
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(-90.0),
+            Angle::ZERO,
+        );
+        let sp = LatLong::from_degrees(-90.0, 50.0);
+        assert!(!a.contains_point(sp));
+        assert!(a.polar_closure().contains_point(sp));
+    }
+
+    #[test]
+    fn contains_point_west() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(30.0),
+            Angle::ZERO,
+            Angle::ZERO,
+        );
+        assert!(a.contains_point(LatLong::from_degrees(10.0, 0.0)));
+    }
+
+    #[test]
+    fn contains_point_date_line() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(-170.0),
+            Angle::ZERO,
+            Angle::from_degrees(170.0),
+        );
+        assert!(a.contains_point(LatLong::from_degrees(10.0, 180.0)));
+        assert!(a.contains_point(LatLong::from_degrees(10.0, -180.0)));
+    }
+
+    #[test]
+    fn contains_point_empty() {
+        assert!(!Rectangle::empty().contains_point(LatLong::from_degrees(0.0, 0.0)));
+    }
+
+    #[test]
+    fn contains_point_empty_longitude_interval() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(-180.0),
+            Angle::ZERO,
+            Angle::from_degrees(180.0),
+        );
+        assert!(!a.contains_point(LatLong::from_degrees(10.0, 180.0)));
+    }
+
+    #[test]
+    fn contains_point_inverted() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::ZERO,
+            Angle::ZERO,
+            Angle::from_degrees(30.0),
+        );
+        assert!(a.contains_point(LatLong::from_degrees(10.0, 40.0)));
+
+        // latitude above north.
+        assert!(!a.contains_point(LatLong::from_degrees(40.0, 40.0)));
+
+        // latitude below south.
+        assert!(!a.contains_point(LatLong::from_degrees(-1.0, 40.0)));
+
+        // outside longitude.
+        assert!(!a.contains_point(LatLong::from_degrees(10.0, 10.0)));
+    }
+
+    #[test]
+    fn contains_point_nominal() {
+        let a = Rectangle::from_nesw(
+            Angle::from_degrees(30.0),
+            Angle::from_degrees(30.0),
+            Angle::ZERO,
+            Angle::ZERO,
+        );
+        assert!(a.contains_point(LatLong::from_degrees(10.0, 10.0)));
+
+        // latitude above north.
+        assert!(!a.contains_point(LatLong::from_degrees(40.0, 10.0)));
+
+        // latitude below south.
+        assert!(!a.contains_point(LatLong::from_degrees(-1.0, 10.0)));
+
+        // longitude after east.
+        assert!(!a.contains_point(LatLong::from_degrees(10.0, 40.0)));
+
+        // longitude after west.
+        assert!(!a.contains_point(LatLong::from_degrees(10.0, -10.0)));
     }
 }
